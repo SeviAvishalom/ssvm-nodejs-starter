3b0f737cf5fa157a87a79cdcc5f4d6d425904e57
SeviAvishalom <Avishalom.sevi@gmail.com>
2020-08-02 14:39:09 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -7,12 +6,3 @@ pub fn say(s: &str) -> String {
     let r = String::from("This is my first Rust program! Hello ");
     return r + s;
 }
-=======
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  let r = String::from("hello ");
-  return r + s;
-}
->>>>>>> e2b488f359c8c4944c6ed9f84d7b737d00ce2f82
